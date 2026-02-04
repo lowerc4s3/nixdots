efb77643f1842ad55ebfafa4156cc5c8a2f9f074
lowerc4s3 <kkirill03@gmail.com>
2026-02-04 23:32:16 +0300

    fix: merge neovide settings

--- modules/software/develop/neovide.nix
+++ modules/software/develop/neovide.nix
@@ -9,33 +9,36 @@
     programs.neovide = {
       enable = true;
 
-      settings.fonts = let
-        family = "JetBrainsMono Nerd Font";
-      in {
-        size = 12;
-        normal = {
-          inherit family;
-          style = "Regular";
-        };
-        bold = {
-          inherit family;
-          style = "Bold";
-        };
-        italic = {
-          inherit family;
-          style = "Medium Italic";
-        };
-        bold_italic = {
-          inherit family;
-          style = "Bold Italic";
-        };
-      };
-
-      settings = lib.mkIf pkgs.stdenv.isDarwin {
-        frame = "buttonless";
-        title-hidden = true;
-        srgb = true;
-      };
+      settings = lib.mkMerge [
+        {
+          fonts = let
+            family = "JetBrainsMono Nerd Font";
+          in {
+            size = 12;
+            normal = {
+              inherit family;
+              style = "Regular";
+            };
+            bold = {
+              inherit family;
+              style = "Bold";
+            };
+            italic = {
+              inherit family;
+              style = "Medium Italic";
+            };
+            bold_italic = {
+              inherit family;
+              style = "Bold Italic";
+            };
+          };
+        }
+        (lib.mkIf pkgs.stdenv.isDarwin {
+          frame = "buttonless";
+          title-hidden = true;
+          srgb = true;
+        })
+      ];
     };
   };
 }
