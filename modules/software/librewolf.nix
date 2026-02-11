aab46e47257593c8c5db2b562e9e03814b146682
lowerc4s3 <kkirill03@gmail.com>
2026-02-11 15:21:05 +0300

    feat: librewolf new config

--- modules/software/librewolf.nix
+++ modules/software/librewolf.nix
@@ -1,82 +1,81 @@
-let
-  policies = {
-    DontCheckDefaultBrowser = true;
-    DisablePocket = true;
-    DisableAppUpdate = true;
-  };
-  settings = {
-    # FIXME: some of these options is wrong
-    # Disabling some librewolf security params
-    # to make web more usable
-    "webgl.disabled" = true;
-    "privacy.resistFingerprinting" = false;
-    "privacy.clearOnShutdown_v2.cache" = false;
-    "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
-
-    # okay, these params are taken from hlissner's nix config
-    # Allow svgs to take on theme colors
-    "svg.context-properties.content.enabled" = true;
-    # Seriously. Stop popping up on every damn page. If I want it translated,
-    # I know where to find gtranslate/deepl/whatever!
-    "browser.translations.automaticallyPopup" = false;
-    # Enable ETP for decent security (makes firefox containers and many
-    # common security/privacy add-ons redundant).
-    "browser.contentblocking.category" = "strict";
-    "privacy.purge_trackers.enabled" = true;
-    "privacy.donottrackheader.enabled" = true;
-    "privacy.donottrackheader.value" = 1;
-    # Enable userContent.css and userChrome.css for our theme modules
-    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-    # Do not check if Firefox is the default browser
-    "browser.shell.checkDefaultBrowser" = false;
-    # Disable Activity Stream
-    # https://wiki.mozilla.org/Firefox/Activity_Stream
-    "browser.newtabpage.activity-stream.enabled" = false;
-    "browser.newtabpage.activity-stream.telemetry" = false;
-    # "Looks like you haven't started Firefox in a while."
-    "browser.disableResetPrompt" = true;
-    # "New to Firefox? Let's get started!" tour
-    "browser.onboarding.enabled" = false;
-    # Reduce File IO / SSD abuse
-    # Otherwise, Firefox bombards the HD with writes. Not so nice for SSDs.
-    # This forces it to write every minute, rather than 15 seconds.
-    "browser.sessionstore.interval" = "30000";
-    # Disable telemetry
-    "toolkit.telemetry.unified" = false;
-    "toolkit.telemetry.enabled" = false;
-    "toolkit.telemetry.server" = "data:,";
-    "toolkit.telemetry.archive.enabled" = false;
-    "toolkit.telemetry.coverage.opt-out" = true;
-    "toolkit.coverage.opt-out" = true;
-    "toolkit.coverage.endpoint.base" = "";
-    "experiments.supported" = false;
-    "experiments.enabled" = false;
-    "experiments.manifest.uri" = "";
-    "browser.ping-centre.telemetry" = false;
-    # Disable health reports (basically more telemetry)
-    "datareporting.healthreport.uploadEnabled" = false;
-    "datareporting.healthreport.service.enabled" = false;
-    "datareporting.policy.dataSubmissionEnabled" = false;
-  };
-in {
+{
   flake.modules.nixos.librewolf = {pkgs, ...}: {
-    # nixpkgs doesn't have librewolf options,
-    # so we configure firefox and pass
-    # librewolf package to use
     programs.firefox = {
       enable = true;
       package = pkgs.librewolf;
-      inherit policies;
-      preferences = settings;
     };
   };
 
-  flake.modules.homeManager.librewolf = {
+  flake.modules.homeManager.librewolf = {pkgs, ...}: {
     programs.librewolf = {
       enable = true;
-      inherit policies;
-      inherit settings;
+      languagePacks = ["ru"];
+      profiles.default = {
+        settings = {
+          # disable some librewolf privacy options
+          # to make web a bit more usable
+          "webgl.disabled" = false;
+          "privacy.resistFingerprinting" = false;
+          "privacy.clearOnShutdown.cache" = false;
+          "privacy.clearOnShutdown.cookies" = false;
+          "privacy.clearOnShutdown_v2.cache" = false;
+          "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+
+          "browser.toolbars.bookmarks.visibility" = "never";
+
+          # restore tabs on startup
+          "browser.sessionstore.enabled" = true;
+          "browser.startup.page" = 3;
+
+          # enable search suggestions
+          "browser.search.suggest.enabled" = true;
+          "browser.urlbar.suggest.searches" = true;
+
+          # use russian locale and ui lang
+          "intl.regional_prefs.use_os_locales" = true;
+
+          # allow svgs to take on theme colors
+          "svg.context-properties.content.enabled" = true;
+
+          # disable page translation
+          "browser.translations.automaticallyPopup" = false;
+
+          # enable ETP for decent security (makes firefox containers and many
+          # common security/privacy add-ons redundant).
+          "browser.contentblocking.category" = "strict";
+          "privacy.purge_trackers.enabled" = true;
+
+          # enable userChrome.css
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+          # "Looks like you haven't started Firefox in a while."
+          "browser.disableResetPrompt" = true;
+          # "New to Firefox? Let's get started!" tour
+          "browser.onboarding.enabled" = false;
+
+          # reduce file IO / SSD abuse
+          # otherwise, firefox bombards the HD with writes. not so nice for SSDs.
+          # this forces it to write every minute, rather than 15 seconds.
+          "browser.sessionstore.interval" = "30000";
+
+          # disable telemetry
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.server" = "data:,";
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.coverage.opt-out" = true;
+          "toolkit.coverage.opt-out" = true;
+          "toolkit.coverage.endpoint.base" = "";
+          "experiments.supported" = false;
+          "experiments.enabled" = false;
+          "experiments.manifest.uri" = "";
+          "browser.ping-centre.telemetry" = false;
+          # disable health reports (basically more telemetry)
+          "datareporting.healthreport.uploadEnabled" = false;
+          "datareporting.healthreport.service.enabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+        };
+      };
     };
-    # TODO: theme, extensions, tridactyl config
   };
 }
