95dc191b813292668f584a5f276e7e7e5508101a
lowerc4s3 <kkirill03@gmail.com>
2026-02-12 16:53:30 +0300

    feat: use latest nvidia driver

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -1,9 +1,12 @@
 {lib, ...}: {
-  flake.modules.nixos.nvidia = {
+  flake.modules.nixos.nvidia = {config, ...}: {
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia = {
+      # NOTE: the default stable driver is a bit stale
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+
       modesetting.enable = true;
-      open = lib.mkDefault true;
+      open = true;
       nvidiaSettings = true;
 
       # Enable nvidia-suspend/hibernate/resume services
