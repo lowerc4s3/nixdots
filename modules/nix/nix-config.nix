f288e8738e72a9994de33fc3f4ef7c8d553fb2a7
lowerc4s3 <kkirill03@gmail.com>
2026-02-24 22:13:34 +0300

    feat: add self to trusted users

--- modules/nix/nix-config.nix
+++ modules/nix/nix-config.nix
@@ -4,6 +4,8 @@ let
     nix.settings = {
       experimental-features = ["flakes" "nix-command" "pipe-operators"];
 
+      trusted-users = ["lowerc4s3"];
+
       # Detect duplicate files in nix store
       # and replace them with hard links
       auto-optimise-store = true;
