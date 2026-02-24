8778e7601d06bdf13524a25c64692a5b1e998d87
lowerc4s3 <kkirill03@gmail.com>
2026-02-24 22:57:12 +0300

    feat: add wl-clipboard

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -1,5 +1,9 @@
 {
-  flake.modules.nixos.kde = {
+  flake.modules.nixos.kde = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+      # NOTE: temp location, will move somewhere else later
+      wl-clipboard
+    ];
     services.desktopManager.plasma6 = {
       enable = true;
       enableQt5Integration = true;
