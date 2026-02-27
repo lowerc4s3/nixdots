f0a2ca5651066da73ecc9e71a64c0e7ec00c00a8
lowerc4s3 <kkirill03@gmail.com>
2026-02-27 20:04:46 +0300

    fix: broken jetbrains mono build

--- modules/nix/nix-config.nix
+++ modules/nix/nix-config.nix
@@ -1,6 +1,22 @@
 {inputs, ...}: let
   shared = {
+    # TODO: move nixpkgs config into perSystem
     nixpkgs.config.allowUnfree = true;
+    nixpkgs.overlays = [
+      # NOTE: this overlay fixes broken picosvg's tests which break jetbrains
+      # mono build (see https://github.com/NixOS/nixpkgs/issues/493679)
+      (final: prev: {
+        pythonPackagesExtensions =
+          prev.pythonPackagesExtensions
+          ++ [
+            (python-final: python-prev: {
+              picosvg = python-prev.picosvg.overridePythonAttrs (oldAttrs: {
+                doCheck = false;
+              });
+            })
+          ];
+      })
+    ];
     nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
     nix.settings = {
       experimental-features = ["flakes" "nix-command" "pipe-operators"];
