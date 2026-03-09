9d8efcab317e0d5f8105c2095bb8ecf9b8ea1365
lowerc4s3 <kkirill03@gmail.com>
2026-03-09 21:09:54 +0300

    refactor(clash-verge): tweak default service instead of using own

--- modules/apps/core/clash-verge.nix
+++ modules/apps/core/clash-verge.nix
@@ -1,51 +1,20 @@
 {
-  flake.modules.nixos.apps-core = {config, ...}: {
+  flake.modules.nixos.apps-core = {lib, ...}: {
     programs.clash-verge = {
       enable = true;
       autoStart = true;
-      serviceMode = false;
+      serviceMode = true;
       tunMode = true;
     };
 
-    # NOTE: using own clash verge service to allow process name routing
-    # (see https://github.com/MetaCubeX/mihomo/issues/961)
-    systemd.services.clash-verge = let
+    # NOTE: appending additional capabilities to clash verge service to allow
+    # process name routing (see https://github.com/MetaCubeX/mihomo/issues/961)
+    networking.firewall.trustedInterfaces = ["Mihomo"];
+    systemd.services.clash-verge.serviceConfig = let
       caps = ["CAP_NET_ADMIN CAP_NET_RAW CAP_SYS_ADMIN CAP_DAC_OVERRIDE CAP_SETUID CAP_SETGID CAP_CHOWN CAP_MKNOD CAP_SYS_PTRACE CAP_DAC_READ_SEARCH"];
     in {
-      enable = true;
-      description = "Clash Verge Service";
-      serviceConfig = {
-        ExecStart = "${config.programs.clash-verge.package}/bin/clash-verge-service";
-        Restart = "on-failure";
-        ProtectSystem = "strict";
-        NoNewPrivileges = true;
-        ProtectHostname = true;
-        SystemCallArchitectures = "native";
-        PrivateTmp = true;
-        PrivateMounts = true;
-        ProtectKernelTunables = true;
-        ProtectKernelModules = true;
-        ProtectKernelLogs = true;
-        ProtectControlGroups = true;
-        LockPersonality = true;
-        RestrictRealtime = true;
-        RuntimeDirectory = "clash-verge-rev";
-        ProtectClock = true;
-        MemoryDenyWriteExecute = true;
-        RestrictSUIDSGID = true;
-        RestrictNamespaces = ["~user cgroup mnt uts"];
-        RestrictAddressFamilies = [
-          "AF_INET AF_INET6 AF_NETLINK AF_PACKET AF_UNIX"
-        ];
-        CapabilityBoundingSet = caps;
-        AmbientCapabilities = caps;
-        SystemCallFilter = [
-          "~@aio @chown @clock @cpu-emulation @debug @keyring @memlock @module @mount @obsolete @pkey @privileged @raw-io @reboot @sandbox @setuid @swap @timer"
-        ];
-        SystemCallErrorNumber = "EPERM";
-      };
-      wantedBy = ["multi-user.target"];
+      CapabilityBoundingSet = lib.mkForce caps;
+      AmbientCapabilities = lib.mkForce caps;
     };
-    networking.firewall.trustedInterfaces = ["Mihomo"];
   };
 }
