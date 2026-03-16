f6fe5c336b09e1121b0caff2d264074603d5582f
lowerc4s3 <kkirill03@gmail.com>
2026-03-17 00:48:59 +0300

    feat(nautilus): set as a default file manager

--- modules/desktop/wm-utils/nautilus.nix
+++ modules/desktop/wm-utils/nautilus.nix
@@ -10,5 +10,11 @@
         terminal = "foot";
       };
     };
+
+    homeManager = {
+      xdg.mimeApps.defaultApplications = {
+        "inode/directory" = "org.gnome.Nautilus.desktop";
+      };
+    };
   };
 }
