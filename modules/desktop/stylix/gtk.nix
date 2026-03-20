bb38f63ec339d21c9ce992ada71e77429f5e7d1e
lowerc4s3 <kkirill03@gmail.com>
2026-03-20 22:45:27 +0300

    fix(gtk): missing gsettings schemas

--- modules/desktop/stylix/gtk.nix
+++ modules/desktop/stylix/gtk.nix
@@ -1,10 +1,15 @@
 {
   flake.aspects.desktop-stylix = {
     nixos = {pkgs, ...}: {
+      programs.dconf.enable = true;
       environment.systemPackages = with pkgs; [
+        gtk3
         adwaita-icon-theme
         adwaita-icon-theme-legacy
       ];
+      environment.variables = {
+        GSETTINGS_SCHEMA_DIR = "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
+      };
     };
 
     homeManager = {
