a8ddef95a48932a6146a7214aaa390e4fd88405e
lowerc4s3 <kkirill03@gmail.com>
2026-07-02 22:09:47 +0300

    add trashy (trash-cli)

--- modules/desktop/core/xdg.nix
+++ modules/desktop/core/xdg.nix
@@ -1,5 +1,11 @@
 {
   flake.aspects.desktop-core = {
+    nixos = {pkgs, ...}: {
+      environment.systemPackages = with pkgs; [
+        trashy
+      ];
+    };
+
     homeManager = {
       xdg = {
         enable = true;
