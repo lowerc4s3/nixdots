e6d79880bbc165d91cb04367542865a6e9725f76
lowerc4s3 <kkirill03@gmail.com>
2026-07-03 23:44:57 +0300

    add fennel syntax for bat

--- modules/cli/bat.nix
+++ modules/cli/bat.nix
@@ -1,11 +1,22 @@
 {
   flake.aspects.cli-bat = {
-    homeManager = {
+    homeManager = {pkgs, ...}: {
       programs.bat = {
         enable = true;
         config = {
           style = "numbers";
           set-terminal-title = true;
+        };
+        syntaxes = {
+          fennel = {
+            src = pkgs.fetchFromGitHub {
+              owner = "gbaptista";
+              repo = "sublime-text-fennel";
+              rev = "212b9d5ab14412d8d1f4f9db783e201785e167d6";
+              hash = "sha256-puHDk0xDvdOfNGOkuc4AqaE/fSNm5vVFqoaFkL1vXIY=";
+            };
+            file = "Fennel.sublime-syntax";
+          };
         };
       };
     };
