957a1458d5c31fdba52a1a0d1740974da5755ee3
lowerc4s3 <kkirill03@gmail.com>
2026-07-17 21:31:01 +0300

    add nix-output-monitor

--- modules/cli/core/nix-utils.nix
+++ modules/cli/core/nix-utils.nix
@@ -1,10 +1,14 @@
 {inputs, ...}: {
   flake.aspects.cli-core = {
-    nixos = {
+    nixos = {pkgs, ...}: {
       imports = [inputs.nix-index-database.nixosModules.default];
 
       # run programs without installing them
       programs.nix-index-database.comma.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        nix-output-monitor
+      ];
     };
   };
 }
