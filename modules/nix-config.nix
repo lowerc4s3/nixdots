80062ca839ef9b099d748852c21701d7aa069a48
lowerc4s3 <kkirill03@gmail.com>
2026-07-22 13:07:32 +0300

    update nix to 2.35

--- modules/nix-config.nix
+++ modules/nix-config.nix
@@ -1,21 +1,22 @@
 {inputs, ...}: {
-  flake.aspects.nix-config = let
-    module = {
+  flake.aspects.nix-config = {
+    nixos = {
       # TODO: move nixpkgs config into perSystem
-      nixpkgs.config.allowUnfree = true;
-      nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-      nix.settings = {
-        experimental-features = ["flakes" "nix-command" "pipe-operators"];
-
-        trusted-users = ["lowerc4s3"];
-
-        # Detect duplicate files in nix store
-        # and replace them with hard links
-        auto-optimise-store = true;
+      nixpkgs = {
+        config.allowUnfree = true;
+        overlays = [
+          # use latest nix version
+          (final: prev: {nix = prev.nixVersions.latest;})
+        ];
+      };
+      nix = {
+        nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+        settings = {
+          experimental-features = ["flakes" "nix-command" "pipe-operators"];
+          trusted-users = ["lowerc4s3"];
+          auto-optimise-store = true;
+        };
       };
     };
-  in {
-    nixos = module;
-    darwin = module;
   };
 }
