45da93fcfa051776cba69fc7d8946f7ab1c75878
lowerc4s3 <kkirill03@gmail.com>
2026-07-28 20:37:52 +0300

    update flake.lock

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -1,10 +1,23 @@
 { inputs, pkgs, ... }:
+let
+  # INFO: broken build already fixed on main as of 28.07.2026,
+  # should be in unstable soon (cached i hope)
+  libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
+    src = pkgs.fetchFromGitLab {
+      domain = "gitlab.freedesktop.org";
+      owner = "emersion";
+      repo = "libdisplay-info";
+      rev = finalAttrs.version;
+      sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+    };
+  });
+in
 {
   imports = [ inputs.niri.nixosModules.niri ];
   niri-flake.cache.enable = false;
   programs.niri = {
     enable = true;
-    package = pkgs.niri;
+    package = pkgs.niri.override { inherit libdisplay-info; };
   };
 
   # disable niri-flake default polkit (kde polkit)
