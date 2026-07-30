be3e6f372f9db513c28bd98a391adfd64511c420
lowerc4s3 <kkirill03@gmail.com>
2026-07-30 22:46:18 +0300

    re-add default apps

--- hosts/lithium/users/lowerc4s3.nix
+++ hosts/lithium/users/lowerc4s3.nix
@@ -35,6 +35,12 @@
 
   xdg.mimeApps.defaultApplications = {
     "inode/directory" = "org.gnome.Nautilus.desktop";
+    "text/html" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+    "application/pdf" = "org.pwmt.zathura.desktop";
   };
 
   home.packages = with pkgs; [
