da595fb03081c7dffcb0c7630985bd98af98ec76
lowerc4s3 <kkirill03@gmail.com>
2026-08-26 18:47:39 +0300

    fix noctalia session buttons

--- modules/home/desktop/noctalia/session.nix
+++ modules/home/desktop/noctalia/session.nix
@@ -8,40 +8,43 @@ let
 in
 {
   config = lib.mkIf (config.atoms.desktop.enable && cfg.enable) {
-    programs.noctalia.settings.session = {
+    programs.noctalia.settings.shell.session = {
       grid = true;
       actions = [
         {
-          action = "lock";
+          action = "lock_and_suspend";
           enabled = true;
           countdown_seconds = 0;
           shortcut = "1";
           variant = "default";
         }
         {
-          action = "lock_and_suspend";
-          enabled = true;
-          countdown_seconds = 0;
-          shortcut = "2";
-          variant = "default";
-        }
-        {
           action = "logout";
           enabled = true;
           countdown_seconds = 5;
-          shortcut = "3";
+          shortcut = "2";
           variant = "default";
         }
         {
           action = "shutdown";
           enabled = true;
           countdown_seconds = 5;
-          shortcut = "4";
+          shortcut = "3";
           variant = "default";
         }
         {
           action = "reboot";
           enabled = true;
+          countdown_seconds = 5;
+          shortcut = "4";
+          variant = "default";
+        }
+        {
+          action = "command";
+          enabled = true;
+          label = "Soft Reboot";
+          glyph = "reload";
+          command = "systemctl soft-reboot";
           countdown_seconds = 5;
           shortcut = "5";
           variant = "default";
